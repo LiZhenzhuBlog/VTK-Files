7edeb081f473c89d95791e6bbc31792071085e76
Abdullah Sikder <abdullahsikder@hotmail.com>
2019-04-01 17:24:45 +1100

    Update mainprogram2.py

--- mainprogram2.py
+++ mainprogram2.py
@@ -1,29 +1,6 @@
 #! usr/bin
 import vtk
 import os
-### ANU SAMPLE FOLDERS ####
-### ANU 10 - Lot10_Yshape_CF8_2AR125.5
-### ANU 11 - Stone1_TwoAR1265_3.4MM_CF8_10um
-### ANU 12 - Stone2_Triangularprism_CF8_2AR126.5
-### ANU 13 - Stone3_Concavesurf_CF8_TwoAR126
-### ANU 14 - Stone4_CF8_2AR126_ReM2Rep1
-### ANU 15 - Stone5_Cuboidroughsurf2_CF8_2AR126
-### ANU 16 - Stone6_Starshape_CF8_TwoAR126
-### ANU 17 - Stone7_Pyramid_CF8_2AR125.5
-### ANU 18 - Stone7_Pyramid_CF8_2AR125.5
-### ANU 19 - Stone9_Smalltriangle_CF8_2AR125.5
-##############################
-##############################
-path10 = "D:/DMY Projects/DiamondSample/ANU10-AT074/Lot10_Yshape_CF8_2AR125.5/"
-path11 = "D:/DMY Projects/DiamondSample/ANU11-AR112/Stone1_TwoAR1265_3.4MM_CF8_10um/"
-path12 = "D:/DMY Projects/DiamondSample/ANU12-AP160RW/Stone2_Triangularprism_CF8_2AR126.5/"
-path13 = "D:/DMY Projects/DiamondSample/ANU13-AP145RW/Stone3_Concavesurf_CF8_TwoAR126/"
-path14 = "D:/DMY Projects/DiamondSample/ANU14-AR371/Stone4_CF8_2AR126_ReM2Rep1/"
-path15 = "D:/DMY Projects/DiamondSample/ANU15-AR392/Stone5_Cuboidroughsurf2_CF8_2AR126/"
-path16 = "D:/DMY Projects/DiamondSample/ANU16-PG0340/Stone6_Starshape_CF8_TwoAR126/"
-path17 = "D:/DMY Projects/DiamondSample/ANU17-PG0349/Stone7_Pyramid_CF8_2AR125.5/"
-path18 = "D:/DMY Projects/DiamondSample/ANU18-PG0155/Stone8_ReM1_DeepFcs_TwoAR1255_CF8/"
-path19 = "D:/DMY Projects/DiamondSample/ANU19-AT021/Stone9_Smalltriangle_CF8_2AR125.5/"
 ##############################
 os.chdir(path16)
 ##############################
@@ -112,15 +89,24 @@ BoxSliceSwitch = False
 CombinedSwitch = False
 ##############################
 
-#Add Jitter Class
+#Add Jitter Class - Optional
 #Add Cut Plane Class - Done
 #Add Volume Slice 3D Class - Done
 #Add Mesh Slice - Done
-#Add Combine Slice to render -Done/Not Accurate
+#Add Combine Slice to render -Done
 
 ##############################
 # Start Rendering
 ##############################
+renderer = vtk.vtkRenderer()
+render_window = vtk.vtkRenderWindow()
+render_window.AddRenderer(renderer)
+interactor = vtk.vtkRenderWindowInteractor()
+interactor.SetInteractorStyle(vtk.vtkInteractorStyleTrackballCamera())
+
+
+render_window.SetInteractor(interactor)
+renderer.SetBackground(0.2,0.2,0.2)
 ##############################
 ##############################
 # Add Rendering Objects
@@ -135,16 +121,6 @@ CombinedSwitch = False
 #renderer.AddVolume(inxi)
 #renderer.AddVolume(inxc)
 
-#if CombinedSwitch == True:
-    
-    
-
-            
-ren.AddActor(in99)
-ren.AddVolume(inv)
-CombinePlaneWidget.PlaceWidget()
-CombinePlaneWidget.SetInteractor(interactor)
-CombinePlaneWidget.On()
 interactor.Initialize()
 interactor.Start()
 
